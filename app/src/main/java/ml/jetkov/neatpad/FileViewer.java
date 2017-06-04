88e32e9f2b70fabd529bb710d398d35cc95de6a8
Alex Petkovic <ajetkov@gmail.com>
2017-06-04 19:18:04 -0400

    successfully implemented fragments

--- app/src/main/java/ml/jetkov/neatpad/FileViewer.java
+++ app/src/main/java/ml/jetkov/neatpad/FileViewer.java
@@ -1,17 +1,37 @@
 package ml.jetkov.neatpad;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class FileViewer extends FragmentActivity {
+public class FileViewer extends AppCompatActivity implements TextEditor.OnFragmentInteractionListener, HTMLViewer.OnFragmentInteractionListener {
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_text_editor);
+        setContentView(R.layout.activity_file_viewer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
