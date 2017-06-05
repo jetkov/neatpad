6f467d5111097dcb645e172ba4c73d29d620f099
Alex Petkovic <ajetkov@gmail.com>
2017-06-04 22:42:56 -0400

    working fragment switching

--- app/src/main/java/ml/jetkov/neatpad/FileViewer.java
+++ app/src/main/java/ml/jetkov/neatpad/FileViewer.java
@@ -1,18 +1,20 @@
 package ml.jetkov.neatpad;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class FileViewer extends AppCompatActivity implements TextEditor.OnFragmentInteractionListener, HTMLViewer.OnFragmentInteractionListener {
+
+    private FragmentManager fragManager = getSupportFragmentManager();
+
+    private TextEditor textEditor = new TextEditor();
+    private HTMLViewer htmlViewer = new HTMLViewer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +23,25 @@ public class FileViewer extends AppCompatActivity implements TextEditor.OnFragme
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        fragManager.beginTransaction().add(R.id.frag_container, textEditor).commit();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                switchViewMode();
             }
         });
+    }
+
+    protected void switchViewMode() {
+        FragmentTransaction fragTransaction = fragManager.beginTransaction();
+        if (findViewById(R.id.text_editor) == null) {
+            fragTransaction.replace(R.id.frag_container, textEditor);
+        } else {
+            fragTransaction.replace(R.id.frag_container, htmlViewer);
+        }
+        fragTransaction.commit();
     }
 
     @Override
--- app/src/main/java/ml/jetkov/neatpad/HTMLViewer.java
+++ app/src/main/java/ml/jetkov/neatpad/HTMLViewer.java
@@ -64,7 +64,7 @@ public class HTMLViewer extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_htmlviewer, container, false);
+        return inflater.inflate(R.layout.fragment_html_viewer, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
