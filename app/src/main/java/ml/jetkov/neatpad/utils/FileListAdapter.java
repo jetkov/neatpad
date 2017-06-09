229aeda397723d0f70826d28e72393e2cf446120
Alex Petkovic <ajetkov@gmail.com>
2017-06-08 21:03:39 -0400

    Added GNU GPLv3.0 Licence Headers

---
+++ app/src/main/java/ml/jetkov/neatpad/utils/FileListAdapter.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2017  Alex Petkovic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ml.jetkov.neatpad.utils;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.io.File;
+
+/**
+ * Created by jetkov on 08/06/17.
+ */
+public class FileListAdapter extends ArrayAdapter<File> {
+
+    FileListAdapter(Context context, File[] files) {
+        super(context, -1, files);
+    }
+
+
+}
